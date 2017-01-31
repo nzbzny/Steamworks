4f5e727a65b726f37b9098c3e49185ab007fa9d2
Noah Z <nzbzny@users.noreply.github.com>
2017-01-30 21:35:26 -0500

    Update IR.h

--- IR.h
+++ IR.h
@@ -1,1 +1,15 @@
-//use to flip the IR values
+#include "WPILib.h"
+
+#ifndef SRC_IR_H
+#define SRC_IR_H
+
+class IR {
+
+  DigitalInput ir;
+  
+public:
+	IR(int channel);
+  void get();
+};
+
+#endif
