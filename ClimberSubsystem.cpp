39745f9029c16beafb06dadd3386573836f840ed
Noah Z <nzbzny@users.noreply.github.com>
2017-02-01 18:59:16 -0500

    Add files via upload

--- ClimberSubsystem.cpp
+++ ClimberSubsystem.cpp
@@ -1,1 +1,11 @@
 #include "ClimberSubsystem.h"
+
+ClimberSubsystem::ClimberSubsystem(int climberChannel) :
+	climber(climberChannel) //initialize climber
+{
+	climber.SetControlMode(CANTalon::ControlMode::kSpeed); //speed control mode - RPM
+}
+
+void ClimberSubsystem::climb() {
+	climber.Set(.5 * Constants::climberMaxSpeed); //TODO: temp value
+}
--- ClimberSubsystem.h
+++ ClimberSubsystem.h
@@ -1,14 +1,16 @@
 #include "WPILib.h"
+#include "CANTalon.h"
+#include "Constants.h"
 
 #ifndef SRC_CLIMBERSUBSYSTEM_H
 #define SRC_CLIMBERSUBSYSTEM_H
 
 class ClimberSubsystem {
 
-
+	CANTalon climber;
 public:
-	ClimberSubsystem();
-
+	ClimberSubsystem(int climberChannel);
+	void climb();
 };
 
 #endif
